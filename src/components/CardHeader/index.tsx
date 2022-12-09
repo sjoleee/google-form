c5e6d2dd58c86258e813290d0db2f878714786b5
sjoleee <cent7425@gmail.com>
2022-12-09 17:33:52 +0900

    feat: :sparkles: 설문 문항의 타입을 수정할 수 있는 inputTypeSelecter 구현. 변경시 store에 반영

--- src/components/CardHeader/index.tsx
+++ src/components/CardHeader/index.tsx
@@ -1,6 +1,9 @@
+import { MenuItem, SelectChangeEvent } from "@mui/material";
 import React, { useEffect } from "react";
 import { useForm, Controller } from "react-hook-form";
+import { useDispatch } from "react-redux";
 
+import { inputTypes, typeChange } from "../../store";
 import * as S from "./styles";
 
 const CardHeader = ({
@@ -15,26 +18,49 @@ const CardHeader = ({
   title: string;
 }) => {
   const { control, setFocus, register } = useForm();
+  const dispatch = useDispatch();
+
   useEffect(() => {
     if (isFocused) setFocus(id);
   }, [isFocused]);
 
+  const handleChange = (e: SelectChangeEvent<unknown>, child: React.ReactNode) => {
+    dispatch(typeChange({ id, inputType: e.target.value as string }));
+  };
+
   return (
-    <Controller
-      name="CardTitle"
-      control={control}
-      render={() => (
-        <S.TextField
-          {...register(id)}
-          id="filled-basic"
-          $isTitle={isTitle}
-          $isFocused={isFocused}
-          defaultValue={title}
-          placeholder={isTitle ? "설문지 제목" : "질문"}
-          variant="filled"
+    <S.Container>
+      <Controller
+        name="CardTitle"
+        control={control}
+        render={() => (
+          <S.TextField
+            {...register(id)}
+            id="filled-basic"
+            $isTitle={isTitle}
+            $isFocused={isFocused}
+            defaultValue={title}
+            placeholder={isTitle ? "설문지 제목" : "질문"}
+            variant="filled"
+          />
+        )}
+      />
+      {!isTitle && isFocused ? (
+        <Controller
+          name="inputTypeSelect"
+          control={control}
+          render={() => (
+            <S.Select onChange={handleChange} defaultValue={inputTypes.RADIO}>
+              <MenuItem value={inputTypes.TEXT}>단답형</MenuItem>
+              <MenuItem value={inputTypes.TEXTAREA}>장문형</MenuItem>
+              <MenuItem value={inputTypes.RADIO}>객관식 질문</MenuItem>
+              <MenuItem value={inputTypes.CHECKBOX}>체크박스</MenuItem>
+              <MenuItem value={inputTypes.SELECT}>드롭다운</MenuItem>
+            </S.Select>
+          )}
         />
-      )}
-    />
+      ) : null}
+    </S.Container>
   );
 };
 
