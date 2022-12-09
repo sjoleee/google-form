5ebcdb97fcb2d10261b4e542dcbe69517f529d13
sjoleee <cent7425@gmail.com>
2022-12-10 00:48:34 +0900

    feat: :sparkles: TextFieldSection 컴포넌트 작성, inputType에 따른 스타일 및 로직 분기처리

---
+++ src/components/TextFieldSection/styles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { TextField as MuiTextField } from "@mui/material";
+
+import { inputTypes } from "../../store";
+
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const TextField = styled(MuiTextField)<{
+  $isTitle: boolean;
+  $isFocused: boolean;
+  $inputType: string;
+}>`
+  width: ${({ $isTitle, $inputType }) => {
+    if ($isTitle) return "100%";
+    if ($inputType === inputTypes.TEXT) return "365px";
+    return "620px";
+  }};
+
+  div {
+    font-size: 14px;
+
+    ::before {
+      border-bottom: ${({ $isTitle, $isFocused, theme }) => {
+        if ($isTitle) {
+          return $isFocused ? `1px solid ${theme.colors.GREY_HEAVY}` : "none";
+        }
+        return `1px dotted ${theme.colors.GREY_HEAVY}`;
+      }} !important;
+    }
+
+    ::after {
+      border-bottom: ${({ theme }) => `2px solid ${theme.colors.PURPLE_HEAVY}`};
+    }
+  }
+`;
