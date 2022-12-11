221aee02c89a96c6ef3b9e7b2fd09c3fbc16d20a
sjoleee <cent7425@gmail.com>
2022-12-12 04:04:46 +0900

    feat: :sparkles: 제출버튼 클릭 시 결과페이지로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Form from "./pages/Form";
 import Preview from "./pages/Preview";
+import Result from "./pages/Result";
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Form />} />
       <Route path="/preview" element={<Preview />} />
+      <Route path="/preview/result" element={<Result />} />
     </Routes>
   </BrowserRouter>
 );
