7f731c82c08ae5693d35a4cdac1c0db9b66ac037
sjoleee <cent7425@gmail.com>
2022-12-11 15:00:44 +0900

    feat: :sparkles: FormProvider 작성

--- src/pages/Preview/index.tsx
+++ src/pages/Preview/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useForm, FormProvider } from "react-hook-form";
 
 import PreviewCard from "../../components/PreviewCard";
 import { StateProps } from "../../store";
 
 const Preview = () => {
   const { cards } = useSelector((state: StateProps) => state);
+  const methods = useForm();
 
   return (
-    <div>
-      {cards.map((card) => (
-        <PreviewCard key={card.id} id={card.id} />
-      ))}
-    </div>
+    <FormProvider {...methods}>
+      <div>
+        {cards.map((card) => (
+          <PreviewCard key={card.id} id={card.id} />
+        ))}
+      </div>
+    </FormProvider>
   );
 };
 
