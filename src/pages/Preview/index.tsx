23b43fa5f625b9b0f4513d97deab4ac4374f51b5
sjoleee <cent7425@gmail.com>
2022-12-12 18:16:18 +0900

    refactor: :recycle: SubmitButton 컴포넌트 제거

--- src/pages/Preview/index.tsx
+++ src/pages/Preview/index.tsx
@@ -1,33 +1,38 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useForm, FormProvider } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 import PreviewCard from "../../components/PreviewCard";
 import { StateProps } from "../../store";
-import SubmitButton from "../../components/SubmitButton";
 import * as S from "./styles";
 
 const Preview = () => {
   const { cards } = useSelector((state: StateProps) => state);
   const methods = useForm();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/preview/result", { state: methods.getValues() });
+  };
 
   return (
     <FormProvider {...methods}>
-      <div>
+      <form onSubmit={methods.handleSubmit(handleClick)}>
         {cards.map((card) => (
           <PreviewCard key={card.id} id={card.id} />
         ))}
-      </div>
-      <S.PreviewSubmitSection>
-        <SubmitButton />
-        <S.ClearButton
-          onClick={() => {
-            window.location.reload();
-          }}
-        >
-          양식 지우기
-        </S.ClearButton>
-      </S.PreviewSubmitSection>
+        <S.PreviewSubmitSection>
+          <S.SubmitButton type="submit">제출</S.SubmitButton>
+          <S.ClearButton
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            양식 지우기
+          </S.ClearButton>
+        </S.PreviewSubmitSection>
+      </form>
     </FormProvider>
   );
 };
